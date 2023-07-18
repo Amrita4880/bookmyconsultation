c57edf3ae911f11a38c84585ffedb8813875463c
Amrita Pandey <amritait3009@gmail.com>
2023-07-18 13:12:17 +0530

    Added first code

---
+++ src/main/java/com/upgrad/bookmyconsultation/service/AppointmentService.java
@@ -0,0 +1,76 @@
+package com.upgrad.bookmyconsultation.service;
+
+import com.upgrad.bookmyconsultation.entity.Appointment;
+import com.upgrad.bookmyconsultation.exception.InvalidInputException;
+import com.upgrad.bookmyconsultation.exception.ResourceUnAvailableException;
+import com.upgrad.bookmyconsultation.exception.SlotUnavailableException;
+import com.upgrad.bookmyconsultation.repository.AppointmentRepository;
+import com.upgrad.bookmyconsultation.repository.UserRepository;
+import com.upgrad.bookmyconsultation.util.ValidationUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AppointmentService {
+
+	@Autowired
+	private AppointmentRepository appointmentRepository;
+	//mark it autowired
+	//create an instance of AppointmentRepository called appointmentRepository
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public String bookAppointment(Appointment appointment){
+
+		ValidationUtils validationUtils = new ValidationUtils();
+		try {
+			validationUtils.validate(appointment);
+		} catch (InvalidInputException e) {
+			throw new RuntimeException(e);
+		}
+		Appointment availableAppointment = appointmentRepository.findByDoctorIdAndTimeSlotAndAppointmentDate(appointment.getDoctorId(), appointment.getTimeSlot(), appointment.getAppointmentDate());
+		if(availableAppointment == null){
+			throw new SlotUnavailableException();
+		} else {
+			Appointment savedAppointment = appointmentRepository.save(appointment);
+			return savedAppointment.getAppointmentId();
+		}
+	}
+	//create a method name appointment with the return type of String and parameter of type Appointment
+	//declare exceptions 'SlotUnavailableException' and 'InvalidInputException'
+		//validate the appointment details using the validate method from ValidationUtils class
+		//find if an appointment exists with the same doctor for the same date and time
+		//if the appointment exists throw the SlotUnavailableException
+		//save the appointment details to the database
+		//return the appointment id
+	
+	
+
+
+	//create a method getAppointment of type Appointment with a parameter name appointmentId of type String
+		//Use the appointmentid to get the appointment details
+		//if the appointment exists return the appointment
+		//else throw ResourceUnAvailableException
+		//tip: use Optional.ofNullable(). Use orElseThrow() method when Optional.ofNullable() throws NULL
+	public Appointment getAppointment(String appointmentId){
+		List<Appointment> appointmentList = appointmentRepository.findByAppointmentId(appointmentId);
+
+		Optional.ofNullable(appointmentList)
+				.orElseThrow(() -> new ResourceUnAvailableException());
+
+		if(appointmentList.size() > 0){
+			return appointmentList.get(0);
+		}
+		return null;
+	}
+
+	public List<Appointment> getAppointmentsForUser(String userId) {
+		return appointmentRepository.findByUserId(userId);
+	}
+}
